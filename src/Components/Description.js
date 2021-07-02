a87f0fc4c160c6a75b537c3f50a40d058d47b6a8
cataopriscila <cataopriscila@gmail.com>
2021-07-02 13:42:22 -0300

    Modified Description component to be more responsive

--- src/Components/Description.js
+++ src/Components/Description.js
@@ -6,11 +6,15 @@ export default function Description() {
   return (
     <div className="description justify">
       <Container>
-        <Row className="space">
+        <Row>
           <Col>
-            <img width="100%" src={Description2} alt="farming" />
-          </Col>          
-          <Col>
+            <img
+              className="description-img1"
+              width="40%"
+              src={Description2}
+              alt="farming"
+            />
+
             <p>
               <strong>Organic.o</strong> is an online social network where
               people can create profiles and write about anything regarding
@@ -26,19 +30,23 @@ export default function Description() {
         <Row className="space"></Row>
         <Row>
           <Col>
+            <img
+              className="description-img2"
+              width="30%"
+              src={Description1}
+              alt="picnic"
+            />
+
             <p>
               At <strong>Organic.o</strong>, you can search by location, product
               or product owner, and you will be connected with people
               everywhere: from all over the country to your neighborhood. Don't
               worry, we break the distance!
             </p>
-            <p>Buy directly from the grower! Sell directly to the customer! </p>
             <p>
-              Find the best way of bringing everything together in one place!
+              Buy directly from the grower! Sell directly to the customer! Find
+              the best way of bringing everything together in one place!
             </p>
-          </Col>
-          <Col>
-            <img width="80%" src={Description1} alt="picnic" />
           </Col>
         </Row>
       </Container>
